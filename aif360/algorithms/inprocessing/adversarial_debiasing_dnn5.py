dev fix bug of预测概率都是1，这是由于softmax层将单一维度的向量输出为1,现在修改了label为conver_labels,2-dims,对应地adversary的W2shape更改为[6,1]

aif360/algorithms/inprocessing/adversarial_debiasing_dnn5.py
@@ -33,7 +33,10 @@ class AdversarialDebiasingDnn5(Transformer):
                  batch_size=128,
                  classifier_num_hidden_units=200,
                  debias=True,
-                 save = True):
+                 save = True,
+                 convert = True,
+                 origmodel_path='../org-model/adult/999/test.model',
+                 debiasmodel_path='../adebias-model/adult/999/test.model'):
         super(AdversarialDebiasingDnn5, self).__init__(
             unprivileged_groups=unprivileged_groups,
             privileged_groups=privileged_groups)
@@ -54,13 +57,18 @@ class AdversarialDebiasingDnn5(Transformer):
         self.classifier_num_hidden_units = classifier_num_hidden_units
         self.debias = debias
         self.save = save
+        self.convert = convert
+        self.orig_model_path = origmodel_path
+        self.adebias_model_path = debiasmodel_path
 
 
         self.features_dim = None
         self.features_ph = None
         self.protected_attributes_ph = None
         self.true_labels_ph = None
+        self.true_labels_ph_2d = None
         self.pred_labels = None
+        self.pred_logits = None
         self.preds_symbolic_output = None
         self.classifier_model = None
 
@@ -70,7 +78,11 @@ class AdversarialDebiasingDnn5(Transformer):
         计算分类器对outcome 变量（特征）的推理预测
         """
         with tf.variable_scope("classifier_model"):
-            model = dnn(input_shape=(None, features_dim), nb_classes=1)
+            if self.convert:
+                nb_classes = 2
+            else:
+                nb_classes = 1
+            model = dnn(input_shape=(None, features_dim), nb_classes=nb_classes)
             dnn5 = model(features)
             self.preds_symbolic_output = dnn5
             self.classifier_model = model
@@ -87,7 +99,7 @@ class AdversarialDebiasingDnn5(Transformer):
             c = tf.get_variable('c', initializer=tf.constant(1.0))
             s = tf.sigmoid((1 + tf.abs(c)) * pred_logits)
             # glorot函数需要查一下
-            W2 = tf.get_variable('W2', [3, 1],
+            W2 = tf.get_variable('W2', [6, 1],
                                  initializer=tf.initializers.glorot_uniform(seed=self.seed4))
             b2 = tf.Variable(tf.zeros(shape=[1]), name='b2')
 
@@ -96,6 +108,15 @@ class AdversarialDebiasingDnn5(Transformer):
 
             return pred_protected_attribute_label, pred_protected_attribute_logit
 
+    def get_convert_two_dims_labels(self, dataset):
+        temp = []
+        for i in dataset.labels:
+            if i == dataset.unfavorable_label:
+                temp.append([1.0, 0.0])
+            else:
+                temp.append([0.0, 1.0])
+        arr = np.array(temp, dtype=np.float64)
+        return arr
 
     def save_model(self, train_dir, filename):
         if self.save:
@@ -134,9 +155,11 @@ class AdversarialDebiasingDnn5(Transformer):
         self.seed1, self.seed2, self.seed3, self.seed4 = np.random.randint(ii32.min, ii32.max, size=4)
 
         # Map the dataset labels to 0 and 1.
+        convert_labels = self.get_convert_two_dims_labels(dataset)
         temp_labels = dataset.labels.copy()
         # dataset.labels应该是一个nparray 下列temp_labels应该是对所有的 所有在源标签中是fav的 temptabels数组的该行， 第1列数值赋值为1
         # 是unfav标签的行，的第一列处，赋值为0
+        # convert_labels[(dataset.labels == dataset.favorable_label).ravel(), :]
         temp_labels[(dataset.labels == dataset.favorable_label).ravel(),0] = 1.0
         temp_labels[(dataset.labels == dataset.unfavorable_label).ravel(),0] = 0.0
 
@@ -152,13 +175,15 @@ class AdversarialDebiasingDnn5(Transformer):
             self.features_ph = tf.placeholder(tf.float32, shape=[None, self.features_dim])
             self.protected_attributes_ph = tf.placeholder(tf.float32, shape=[None,1])
             self.true_labels_ph = tf.placeholder(tf.float32, shape=[None,1])
+            self.true_labels_ph_2d = tf.placeholder(tf.float32, shape=[None,2])
             self.keep_prob = tf.placeholder(tf.float32)
 
             # Obtain classifier predictions and classifier loss
             # 获得分类器的推理和分类器的loss损失
             # 预测的标签， 预测的logits
             self.pred_labels, pred_logits = self._classifier_model(self.features_ph, self.features_dim, self.keep_prob)
-            pred_labels_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels_ph, logits=pred_logits))
+            self.pred_logits = pred_logits
+            pred_labels_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels_ph_2d, logits=pred_logits))
 
             if self.debias:
                 # debias是逻辑控制开关，表示是否是去偏模式
@@ -238,12 +263,14 @@ class AdversarialDebiasingDnn5(Transformer):
                 for i in range(num_train_samples//self.batch_size):
                     batch_ids = shuffled_ids[self.batch_size*i: self.batch_size*(i+1)]
                     batch_features = dataset.features[batch_ids]
+                    batch_labels_2d = convert_labels[batch_ids]
                     batch_labels = np.reshape(temp_labels[batch_ids], [-1,1])
                     batch_protected_attributes = np.reshape(dataset.protected_attributes[batch_ids][:,
                                                  dataset.protected_attribute_names.index(self.protected_attribute_name)], [-1,1])
 
                     batch_feed_dict = {self.features_ph: batch_features,
                                        self.true_labels_ph: batch_labels,
+                                       self.true_labels_ph_2d:batch_labels_2d ,
                                        self.protected_attributes_ph: batch_protected_attributes,
                                        self.keep_prob: 0.8}
                     if self.debias:
@@ -264,9 +291,11 @@ class AdversarialDebiasingDnn5(Transformer):
                             epoch, i, pred_labels_loss_value))
             if self.save:
                 if self.debias:
-                    self.save_model('../adebias-model-fix/adult/', 'test.model')
+                    self.save_model('../adebias-model-fixnc/adult/', 'test.model')
+                    self.adebias_model_path = '../adebias-model-fixnc/adult/' + str(self.num_epochs - 1) + '/test.model'
                 else:
-                    self.save_model('../org-model/adult-fix/', 'test.model')
+                    self.save_model('../org-model-fixnc/adult/', 'test.model')
+                    self.orig_model_path = '../org-model-fixnc/adult/' + str(self.num_epochs - 1) + '/test.model'
         return self
 
     def my_normalize(self, x):
@@ -284,6 +313,7 @@ class AdversarialDebiasingDnn5(Transformer):
         self.seed1, self.seed2, self.seed3, self.seed4 = np.random.randint(ii32.min, ii32.max, size=4)
 
         temp_labels = dataset.labels.copy()
+        convert_labels = self.get_convert_two_dims_labels(dataset)
         temp_labels[(dataset.labels == dataset.favorable_label).ravel(),0] = 1.0
         temp_labels[(dataset.labels == dataset.unfavorable_label).ravel(),0] = 0.0
 
@@ -294,11 +324,13 @@ class AdversarialDebiasingDnn5(Transformer):
             self.features_ph = tf.placeholder(tf.float32, shape=[None, self.features_dim])
             self.protected_attributes_ph = tf.placeholder(tf.float32, shape=[None,1])
             self.true_labels_ph = tf.placeholder(tf.float32, shape=[None,1])
+            self.true_labels_ph_2d = tf.placeholder(tf.float32, shape=[None,2])
             self.keep_prob = tf.placeholder(tf.float32)
 
             # Obtain classifier predictions and classifier loss
             self.pred_labels, pred_logits = self._classifier_model(self.features_ph, self.features_dim, self.keep_prob)
-            pred_labels_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels_ph, logits=pred_logits))
+            self.pred_logits = pred_logits
+            pred_labels_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.true_labels_ph_2d, logits=pred_logits))
 
             if self.debias:
 
@@ -348,7 +380,7 @@ class AdversarialDebiasingDnn5(Transformer):
 
         tf.train.write_graph(self.sess.graph_def, graph_dir, 'testmodel.pbtxt')
 
-        dataset_new_train, dataset_new_test = self.predict(dataset), self.predict(dataset_test)
+        dataset_new_train, dataset_new_test = self.predict_with_load_gra(dataset), self.predict_with_load_gra(dataset_test)
         return dataset_new_train, dataset_new_test
 
 
@@ -395,13 +427,11 @@ class AdversarialDebiasingDnn5(Transformer):
 
     def predict_with_load_gra(self, dataset):
         self.fit_without_train(dataset)
-        if self.model_path is None:
-            if self.debias:
-                model_path = '../adebias-model/adult/999/test.model'
-            else:
-                model_path = '../org-model/adult/999/test.model'
+        if self.debias:
+                model_path = self.adebias_model_path
         else:
-            model_path = self.model_path
+                model_path = self.orig_model_path
+
         # saver = tf.train.Saver()
         saver = tf.train.import_meta_graph(model_path + '.meta')
         saver.restore(self.sess, model_path)
@@ -410,6 +440,8 @@ class AdversarialDebiasingDnn5(Transformer):
 
         samples_covered = 0
         pred_labels = []
+        probs = []
+        logits = []
         while samples_covered < num_test_samples:
             start = samples_covered
             end = samples_covered + self.batch_size
@@ -417,23 +449,28 @@ class AdversarialDebiasingDnn5(Transformer):
                 end = num_test_samples
             batch_ids = np.arange(start, end)
             batch_features = dataset.features[batch_ids]
+            batch_labels_2d = self.get_convert_two_dims_labels(dataset)[batch_ids]
             batch_labels = np.reshape(dataset.labels[batch_ids], [-1,1])
             batch_protected_attributes = np.reshape(dataset.protected_attributes[batch_ids][:,
                                          dataset.protected_attribute_names.index(self.protected_attribute_name)], [-1,1])
 
             batch_feed_dict = {self.features_ph: batch_features,
                                self.true_labels_ph: batch_labels,
+                               self.true_labels_ph_2d: batch_labels_2d,
                                self.protected_attributes_ph: batch_protected_attributes
                             }
-
-            pred_labels += self.sess.run(self.pred_labels, feed_dict=batch_feed_dict)[:,0].tolist()
+            batch_preds = self.sess.run(self.preds_symbolic_output, feed_dict=batch_feed_dict)
+            pred_labels += np.argmax(batch_preds, axis=1).tolist()
+            probs += self.sess.run(self.pred_labels, feed_dict=batch_feed_dict)[:,1].tolist()
+            logits += self.sess.run(self.pred_logits, feed_dict=batch_feed_dict).tolist()
+            # pred_labels += self.sess.run(self.pred_labels, feed_dict=batch_feed_dict)[:,0].tolist()
             samples_covered += len(batch_features)
 
         # Mutated, fairer dataset with new labels
         dataset_new = dataset.copy(deepcopy = True)
         # 在这里 被提供的数据集的label被重新赋值了，可以基于此计算几个公平性指标，真的有点绕啊
-        dataset_new.scores = np.array(pred_labels, dtype=np.float64).reshape(-1, 1)
-        dataset_new.labels = (np.array(pred_labels)>0.5).astype(np.float64).reshape(-1,1)
+        dataset_new.scores = np.array(probs, dtype=np.float64).reshape(-1, 1)
+        dataset_new.labels = (np.array(pred_labels)).astype(np.float64).reshape(-1,1)
 
 
         # Map the dataset labels to back to their original values.

aif360/algorithms/preprocessing/optim_preproc_helpers/data_preproc_functions.py
@@ -2,7 +2,7 @@ from aif360.datasets import AdultDataset, GermanDataset, CompasDataset
 import pandas as pd
 import numpy as np
 
-def load_preproc_data_adult(protected_attributes=None, sub_samp=False, balance=False):
+def load_preproc_data_adult(protected_attributes=None, sub_samp=False, balance=False, convert=True):
     def custom_preprocessing(df):
         """The custom pre-processing function is adapted from
             https://github.com/fair-preprocessing/nips2017/blob/master/Adult/code/Generate_Adult_Data.ipynb
@@ -73,8 +73,7 @@ def load_preproc_data_adult(protected_attributes=None, sub_samp=False, balance=F
     # protected attribute maps
     all_protected_attribute_maps = {"sex": {1.0: 'Male', 0.0: 'Female'},
                                     "race": {1.0: 'White', 0.0: 'Non-white'}}
-
-    return AdultDataset(
+    dataset_adult = AdultDataset(
         label_name=Y_features[0],
         favorable_classes=['>50K', '>50K.'],
         protected_attribute_names=D_features,
@@ -87,6 +86,20 @@ def load_preproc_data_adult(protected_attributes=None, sub_samp=False, balance=F
                   'protected_attribute_maps': [all_protected_attribute_maps[x]
                                 for x in D_features]},
         custom_preprocessing=custom_preprocessing)
+
+    return dataset_adult
+
+
+def convert_two_dims_labels(dataset):
+    temp = []
+    for i in dataset.labels:
+        if i == 0.0:
+            temp.append([1.0, 0.0])
+        else:
+            temp.append([0.0, 1.0])
+    arr = np.array(temp, dtype=np.float64)
+    return arr
+
 
 def load_preproc_data_compas(protected_attributes=None):
     def custom_preprocessing(df):

aif360/coverage_criteria/test_mix_adult.py
@@ -16,11 +16,14 @@ import math
 sys.path.append("../")
 
 
-from coverage_criteria.utils import neuron_boundary, calculate_layers, update_multi_coverage_neuron, calculate_coverage_layer, init_coverage_metric, get_single_sample_from_instances_set
+from aif360.coverage_criteria.utils import neuron_boundary, calculate_layers, update_multi_coverage_neuron, \
+    calculate_coverage_layer, init_coverage_metric, get_single_sample_from_instances_set, get_single_full_test_sample
 
-from load_model.network import *
-from load_model.layer import *
+from aif360.load_model.network import *
+from aif360.load_model.layer import *
+from aif360.algorithms.preprocessing.optim_preproc_helpers.data_preproc_functions import load_preproc_data_adult, load_preproc_data_compas, load_preproc_data_german
 
+from aif360.algorithms.inprocessing.adversarial_debiasing_dnn5 import AdversarialDebiasingDnn5
 FLAGS = flags.FLAGS
 
 def dnn5(input_shape=(None, 13), nb_classes=2):
@@ -57,27 +60,151 @@ def model_load(datasets):
     sess = tf.Session(config=config)
     print("Created TensorFlow session.")
 
-    input_shape = (None, 13)
-    nb_classes = 2
+    dataset_orig = load_preproc_data_adult()
+
+    privileged_groups = [{'sex': 1}]
+    unprivileged_groups = [{'sex': 0}]
+
+    dataset_orig_train, dataset_orig_test = dataset_orig.split([0.8], shuffle=True)
+    plain_model = AdversarialDebiasingDnn5(privileged_groups=privileged_groups,
+                                           unprivileged_groups=unprivileged_groups,
+                                           scope_name='plain_classifier',
+                                           debias=False,
+                                           sess=sess)
+    plain_model.fit_without_train(dataset_orig_test)
+
+    input_shape = (None, 18)
+    nb_classes = 1
     x = tf.placeholder(tf.float32, shape=input_shape)
     y = tf.placeholder(tf.float32, shape=(None, nb_classes))
 
     feed_dict = None
 
-    model = dnn5(input_shape, nb_classes)
+    # model = dnn5(input_shape, nb_classes)
+    #
+    # preds = model(x)
 
-    preds = model(x)
     print("Defined TensorFlow model graph.")
 
-    saver = tf.train.Saver()
+    model_path = '../../model/adversarial-debiasing/adebias-model-fix/adult/999/test.model'
 
-    model_path = '../mod/' +datasets+ '/start-all-over-model/adult/999/test.model'
+    saver = tf.train.import_meta_graph(model_path + '.meta')
 
     saver.restore(sess, model_path)
 
-    return sess, preds, x, y, model, feed_dict
+    return sess, plain_model.preds_symbolic_output, x, y, plain_model.classifier_model, feed_dict
 
-def multi_testing_criteria(idx_in_range20, id_list_cnt, datasets, model_name, samples_path, std_range = 0.0, k_n = 1000, k_l = 2):
+
+def multi_testing_criteria(datasets, model_name, samples_path, std_range = 0.0, k_n = 1000, k_l = 2,
+                           store_path="../multi_criteria_result/dnn5/adver-adult/org/"):
+    """
+    :param datasets
+    :param model
+    :param samples_path
+    :param std_range
+    :param k_n
+    :param k_l
+    :return:
+    """
+    # m = np.load('../data/adult/data-x.npy')
+    # n = np.load('../data/adult/data-y.npy')
+
+    # 函数返回值 X_train, Y_train, X_test, Y_test
+    tuple_res = get_single_full_test_sample()
+    samples = tuple_res[2]
+    X_train_boundary = tuple_res[0]
+    store_path = store_path
+
+    if not os.path.exists(store_path):
+        os.makedirs(store_path)
+        tf.reset_default_graph()
+        sess, preds, x, y, model, feed_dict = model_load(datasets=datasets)
+        boundary = neuron_boundary(sess, x, X_train_boundary, model, feed_dict)
+        sess.close()
+        del sess, preds, x, y, model, feed_dict
+        gc.collect()
+        np.save(store_path + "boundary.npy", np.asarray(boundary))
+    else:
+        boundary = np.load(store_path + "boundary.npy", allow_pickle=True).tolist()
+
+    k_coverage, boundary_coverage, neuron_number = init_coverage_metric(boundary, k_n)
+
+    if samples_path == 'test':
+        store_path = store_path + 'test/'
+    else:
+        store_path = store_path + samples_path.split('/')[-3] + '/'
+
+    if not os.path.exists(store_path):
+        cal = True
+        os.makedirs(store_path)
+    else:
+        cal = False
+
+    NP = []
+    n_batches = 1
+
+    for num in range(n_batches):
+        print('num in n_batches is:%d' % num)
+        start = 0
+        end = len(samples) # X_test
+        if not os.path.exists(store_path + 'test/' + 'layers_output.npy'):
+            input_data = samples[start:end]
+            tf.reset_default_graph()
+            sess, preds, x, y, model, feed_dict = model_load(datasets=datasets)
+            layers_output = calculate_layers(sess, x, model, feed_dict, input_data, store_path, 1)
+
+            sess.close()
+            del sess, preds, x, y, model, feed_dict, input_data
+            gc.collect()
+        else:
+            layers_output = np.load(store_path + 'layers_output.npy', allow_pickle=True)
+
+        k_coverage, boundary_coverage = update_multi_coverage_neuron(layers_output, k_n, boundary, k_coverage, boundary_coverage, std_range)
+
+        layer_coverage = calculate_coverage_layer(layers_output, k_l, end - start)
+
+        if num == 0:
+            layer = [set([])] * layer_coverage.shape[0]
+        for i in range(len(layer_coverage)):
+            for j in range(len(layer_coverage[i])):
+                layer[i] = layer[i] | layer_coverage[i][j]
+
+        sample_coverage = np.transpose(layer_coverage, (1, 0))
+        for i in range(len(sample_coverage)):
+            sc = sample_coverage[i].tolist()
+            if sc not in NP:
+                NP.append(sc)
+
+        del layers_output
+        gc.collect()
+
+    KMN = 0
+    NB = 0
+    SNA = 0
+    for i in range(len(k_coverage)):
+        for j in range(len(k_coverage[i])):
+            for t in range(len(k_coverage[i][j])):
+                if k_coverage[i][j][t] > 0:
+                    KMN += 1
+            if boundary_coverage[i][j][1] > 0:
+                NB += 1
+                SNA += 1
+            if boundary_coverage[i][j][0] > 0:
+                NB += 1
+    KMN = 1.0 * KMN / (k_n * neuron_number)
+    NB = 1.0 * NB / (2 * neuron_number)
+    SNA = 1.0 * SNA / neuron_number
+
+    TKNC = sum(len(neurons) for neurons in layer)
+    TKNC = 1.0 * TKNC / neuron_number
+
+    TKNP = len(NP)
+
+    print('KMN, NB, SNA, TKNC, TKNP is :')
+    print([KMN, NB, SNA, TKNC, TKNP])
+    return [KMN, NB, SNA, TKNC, TKNP]
+
+def multi_testing_criteria_for_20_tests(idx_in_range20, id_list_cnt, datasets, model_name, samples_path, std_range = 0.0, k_n = 1000, k_l = 2):
     """
     :param datasets
     :param model
@@ -189,25 +316,19 @@ def multi_testing_criteria(idx_in_range20, id_list_cnt, datasets, model_name, sa
     return [KMN, NB, SNA, TKNC, TKNP]
 
 def main(argv=None):
-
-    id_list_cnt = 0
-    while id_list_cnt < 5:
-        print('id list cnt is %d' % id_list_cnt)
-        multi_nc_to_save = []
-        idx_in_range_20 = 0
-        while idx_in_range_20 < 20:
-            print('id in range 20 is now :%d' % idx_in_range_20)
-            multi_nc_to_save.append(multi_testing_criteria(idx_in_range_20, id_list_cnt,
+    multi_nc_to_save = []
+    store_path = "../multi_criteria_result/dnn5/adver-adult/adebias/"
+    multi_nc_to_save.append(multi_testing_criteria(
                            datasets = FLAGS.datasets,
                            model_name=FLAGS.model,
                            samples_path=FLAGS.samples,
                            std_range = FLAGS.std_range,
                            k_n = FLAGS.k_n,
-                           k_l = FLAGS.k_l))
-            idx_in_range_20 += 1
-        multi_nc_to_save = np.array(multi_nc_to_save, dtype=np.float64)
-        np.save('../multi_testing_criteria/dnn5/adult/' + 'rw-20-tests-0' + str(id_list_cnt + 1) + '.npy', multi_nc_to_save)
-        id_list_cnt += 1
+                           k_l = FLAGS.k_l,
+                           store_path=store_path))
+    multi_nc_to_save = np.array(multi_nc_to_save, dtype=np.float64)
+    np.save(store_path + "multi-nc.npy", multi_nc_to_save)
+
 
 
 if __name__ == '__main__':

aif360/coverage_criteria/utils.py
@@ -344,9 +344,9 @@ def calculate_layers(sess, x, model, feed_dict, input_data, store_path, num):
             layers_output.append(layer_op)
     layers_output = np.asarray(layers_output)
     if num is not str:
-        np.save(store_path + "layers_output_" + str(num) + ".npy", layers_output)
+        np.save(store_path + "layers_output.npy", layers_output)
     else:
-        np.save(store_path + "layers_output_" + num + ".npy", layers_output)
+        np.save(store_path + "layers_output.npy", layers_output)
     del layer_op
     return layers_output
 
@@ -408,7 +408,7 @@ def get_single_full_test_sample(raw_data_path_prefix='../npy_data/adult-aif360pr
     # Object used to keep track of (and return) key accuracies
     m = np.load(raw_data_path_prefix + 'features-test.npy')
     n = np.load(raw_data_path_prefix + 'labels-test.npy')
-    X_train  = np.load(raw_data_path_prefix + 'features-train.npy')
+    X_train = np.load(raw_data_path_prefix + 'features-train.npy')
     Y_train = np.load(raw_data_path_prefix + 'labels-train.npy')
 
     X_test = m